d50a47be3fbb3d3ba0725ba2d7eea774259377a1
zoey <zoey@zoeydeMacBook-Pro.local>
2019-09-10 21:25:29 -0300

    fix new

--- app/jobs/course_job.rb
+++ app/jobs/course_job.rb
@@ -14,30 +14,50 @@ class CourseJob
 
       page = form.submit
 
-      dashboard = page.link_with(text: 'Dashboard').click
+      # dashboard = page.link_with(text: 'Dashboard').click
 
-      bio_ay = []
-      phy_ay =[]
-      cs_ay =[]
-      math_ay=[]
+      cs3420_ay = []
+      cs2910_ay =[]
+      cs2820_ay =[]
+      cs2060_ay =[]
+# page.search('.media-body').each do |e|
+# p e.text.strip
+#   if e.text.strip == '2019F CS-3420-1'
+#     e.parent().parent().parent().attributes['href'].value
+# binding.pry
+#   end
+# end
 
-      bio = dashboard.link_with(text: '2019W Digital Systems (CS-1610-1)').click
-
-      bio.search('.instancename').each do |i|
-         bio_ay.push(i.text.strip)
+      cs3420 = page.link_with(text:"\n                \n                    \n                        \n                            \n                        \n                        2019F CS-3420-1\n                    \n                \n            ").click
+      cs3420.search('.instancename').each do |i|
+         cs3420_ay.push(i.text.strip)
       end
 
-      cs = dashboard.link_with(text: '2019W Computer Science II (CS-1920-2)').click
-
-      cs.search('.instancename').each do |i|
-        cs_ay.push(i.text.strip)
+      cs2910 = page.link_with(text:"\n                \n                    \n                        \n                            \n                        \n                        2019F CS-2910-1\n                    \n                \n            ").click
+      cs2910.search('.instancename').each do |i|
+         cs2910_ay.push(i.text.strip)
       end
 
-      math = dashboard.link_with(text: '2019W Computer Org. and Architecture (CS-2520-1)').click
-
-      math.search('.instancename').each do |i|
-        math_ay.push(i.text.strip)
+      cs2820 = page.link_with(text:"\n                \n                    \n                        \n                            \n                        \n                        2019F CS-2820-1\n                    \n                \n            ").click
+      cs2820.search('.instancename').each do |i|
+         cs2820_ay.push(i.text.strip)
       end
+
+      cs2060 = page.link_with(text:"\n                \n                    \n                        \n                            \n                        \n                        2019F CS-2060-1\n                    \n                \n            ").click
+      cs2060.search('.instancename').each do |i|
+         cs2060_ay.push(i.text.strip)
+      end
+      # cs = dashboard.link_with(text: '2019W Computer Science II (CS-1920-2)').click
+
+      # cs.search('.instancename').each do |i|
+      #   cs_ay.push(i.text.strip)
+      # end
+
+      # math = dashboard.link_with(text: '2019W Computer Org. and Architecture (CS-2520-1)').click
+
+      # math.search('.instancename').each do |i|
+      #   math_ay.push(i.text.strip)
+      # end
 
 
       # phy = dashboard.link_with(text: '2018F Life in the Universe (PHYS-1510-1)').click
@@ -46,54 +66,69 @@ class CourseJob
       #   phy_ay.push(i.text.strip)
       # end
 
-      bio_ay.each do |b|
+      cs3420_ay.each do |b|
         if Course.find_by_title(b).nil?
-          Course.create(title: b,catalog: 3,status: 2)
+          Course.create(title: b,catalog: 5,status: 2)
+        end
+      end
+      cs2910_ay.each do |b|
+        if Course.find_by_title(b).nil?
+          Course.create(title: b,catalog: 6,status: 2)
+        end
+      end
+       cs2820_ay.each do |b|
+        if Course.find_by_title(b).nil?
+          Course.create(title: b,catalog: 7,status: 2)
+        end
+      end
+       cs2060_ay.each do |b|
+        if Course.find_by_title(b).nil?
+          Course.create(title: b,catalog: 8,status: 2)
         end
       end
 
-      # phy_ay.each do |b|
-      #   if Course.find_by_title(b).nil?
-      #     Course.create(title: b,catalog: 2,status: 2)
-      #   end
-      # end
-
-      cs_ay.each do |b|
-        if Course.find_by_title(b).nil?
-          Course.create(title: b,catalog: 4,status: 2)
-        end
-      end
-
-      math_ay.each do |b|
-        if Course.find_by_title(b).nil?
-          Course.create(title: b,catalog: 1,status: 2)
-        end
-      end
 
 
-      if Course.where(catalog: 3,status: 2).count > 0
-          @courses = Course.where(catalog: 3,status: 2)
-          UserMailer.sample_email(@courses,'2019W Digital Systems (CS-1610-1)').deliver
+      if Course.where(catalog: 5,status: 2).count > 0
+          @courses = Course.where(catalog: 5,status: 2)
+          UserMailer.sample_email(@courses,'2019F CS-3420-1').deliver
           @courses.update(status: 1)
       end
 
+      if Course.where(catalog: 6,status: 2).count > 0
+          @courses = Course.where(catalog: 6,status: 2)
+          UserMailer.sample_email(@courses,'2019F CS-2910-1').deliver
+          @courses.update(status: 1)
+      end
+
+      if Course.where(catalog: 7,status: 2).count > 0
+          @courses = Course.where(catalog: 7,status: 2)
+          UserMailer.sample_email(@courses,'2019F CS-2820-1').deliver
+          @courses.update(status: 1)
+      end
+
+      if Course.where(catalog: 8,status: 2).count > 0
+          @courses = Course.where(catalog: 8,status: 2)
+          UserMailer.sample_email(@courses,'2019F CS-2060-1').deliver
+          @courses.update(status: 1)
+      end
       # if Course.where(catalog: 2,status: 2).count > 0
       #     @courses = Course.where(catalog: 2,status: 2)
       #     UserMailer.sample_email(@courses,'Life in the Universe (PHYS-1510-1)').deliver
       #     @courses.update(status: 1)
       # end
 
-      if Course.where(catalog: 4,status: 2).count > 0
-          @courses = Course.where(catalog: 4,status: 2)
-          UserMailer.sample_email(@courses,'2019W Computer Science II (CS-1920-2)').deliver
-          @courses.update(status: 1)
-      end
+      # if Course.where(catalog: 4,status: 2).count > 0
+      #     @courses = Course.where(catalog: 4,status: 2)
+      #     UserMailer.sample_email(@courses,'2019W Computer Science II (CS-1920-2)').deliver
+      #     @courses.update(status: 1)
+      # end
 
-      if Course.where(catalog: 1,status: 2).count > 0
-          @courses = Course.where(catalog: 1,status: 2)
-          UserMailer.sample_email(@courses,'2019W Computer Org. and Architecture (CS-2520-1)').deliver
-          @courses.update(status: 1)
-      end
+      # if Course.where(catalog: 1,status: 2).count > 0
+      #     @courses = Course.where(catalog: 1,status: 2)
+      #     UserMailer.sample_email(@courses,'2019W Computer Org. and Architecture (CS-2520-1)').deliver
+      #     @courses.update(status: 1)
+      # end
     end
 
   end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,12 @@ class Course < ApplicationRecord
     'math':1,
     'phy':2,
     'bio':3,
-    'cs':4
+    'cs':4,
+    'cs3420':5,
+    'cs2910':6,
+    'cs2820':7,
+    'cs2060':8,
+
   }
 
   enum status:{
